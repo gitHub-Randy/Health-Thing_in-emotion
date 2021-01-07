af44915c7082a5ced7d4a85d3ccd1f146b45d05f
Randy <randy.anft@gmail.com>
2021-01-07 16:33:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -41,20 +41,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
-
-
 
 
   
